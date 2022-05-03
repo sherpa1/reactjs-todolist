55f3a5245e30286b192893fa12449d1e2b948d05
Alexandre Leroux <alex@sherpa.one>
2022-05-03 12:36:50 +0200

    add TasksList Component

---
+++ src/components/TasksList.js
@@ -0,0 +1,12 @@
+function TasksList() {
+    return (
+      <div className="TasksList">
+          <ul>
+              <li>A task</li>
+              <li>An other task</li>
+          </ul>
+      </div>
+    );
+  }
+  
+  export default TasksList;
